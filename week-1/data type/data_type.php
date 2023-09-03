332ea74c9d92a1ec27cebbe119a7709f366539b9
Masud Rana <masudranaonline75@gmail.com>
2023-09-03 23:52:46 +0600

    data type with php

---
+++ week-1/data type/data_type.php
@@ -0,0 +1,58 @@
+<?php
+
+//integer
+
+    // echo 10 + 12;
+    // echo gettype(12+10);
+    // print_r(12+465);
+    // var_dump(10);
+
+//float
+
+    // echo 2.5 + 3.5;
+    // var_dump(2.25 + 3.15);
+
+//string
+
+    // echo "Masud Rana";
+    // var_dump('Masud Rana');
+    // echo gettype('Masud Rana');
+    // echo "10+10";
+
+//boolean
+
+    // echo true;
+    // echo false;
+
+//    echo gettype(true);
+    // var_dump(true);
+
+//array
+
+    // array(10, 'Masud Rana', true);
+    // var_dump([10, 'Masud Rana', false]);
+    // print_r([10, 'Masud Rana', false]);
+
+//null
+
+    // echo null;
+    // var_dump(null);
+
+
+//object
+
+    class Animal {
+
+    }
+
+    var_dump(new Animal());
+
+
+
+
+
+
+
+
+
+
