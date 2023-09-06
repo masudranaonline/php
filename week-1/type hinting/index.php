4c9687d9f5571268d45ca38bc7b518ba4fbc3266
Masud Rana <masudranaonline75@gmail.com>
2023-09-07 00:10:18 +0600

    type hunting

---
+++ week-1/type hinting/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    
+    declare(strict_types=1);
+
+    // $age = "19";
+
+    // var_dump($age + 11);
+
+    // $birthyear = 2002;
+
+    function calculateAge(int $birthyear, string $name){
+        echo "hay, $name, you are". 2023 - $birthyear. "years old";
+    }
+    calculateAge(2002, "Masud");
