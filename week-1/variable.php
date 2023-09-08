0649767998eb5fb3a4a526cb483ed2b5c55149cf
Masud Rana <masudranaonline75@gmail.com>
2023-09-08 20:24:07 +0600

    variable

---
+++ week-1/variable.php
@@ -0,0 +1,21 @@
+<?php
+
+
+    // $name = "Masud Rana";
+    // echo $name;
+
+    $username = "masud123";
+
+    // function sayhi() {
+    //     $name = 'Rana';
+    //     global $username;
+    //     echo $username." " .$name;;
+    // }
+
+    // sayhi();
+
+    // echo "<br>";
+
+    // echo $username;
+
+    echo "Hello, my name is Alan and here is my username: $username";
