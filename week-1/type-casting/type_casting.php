9b0ec725a43876f8356b8a3b9a6aed1068086b5e
Masud Rana <masudranaonline75@gmail.com>
2023-09-08 20:05:15 +0600

    type casting

---
+++ week-1/type-casting/type_casting.php
@@ -0,0 +1,59 @@
+<?php
+
+    // $userAge = '25';
+
+    // $minimumage = 18;
+
+    // if((int)$userAge >= $minimumage){
+    //     echo "vote accepted";
+    // }else {
+    //     echo "Sorry you'r too young to vote";
+    // }
+
+    // $string = "Masud";
+
+    // var_dump( (bool)$string);
+    // var_dump( (int)$string);
+    // var_dump( (float)$string);
+
+    // $str = "21 years old";
+
+    // var_dump((int)$str);
+    // var_dump((float)$str);
+    // var_dump((array)$str);
+
+    // $str = "null";
+
+    // var_dump((bool)$str);
+
+    // $num = 1546.35124;
+
+    // var_dump((int)$num);
+    // var_dump((float)$num);
+
+    // $stock = true;
+
+    // var_dump((string)$stock);
+
+    // $stock = [12,45,12,45,23];
+
+    // var_dump((string)$stock);
+    // $stock = "Apple";
+    // var_dump((array)$stock);
+
+
+    // class frut {
+    //     public $username;
+    //     private $fathername;
+    //     protected $mothername;
+    // }
+
+    // var_dump((array) new frut());
+
+    $str = (object)["name" => "masud", "fruit" => "Apple", "vagitavle" => "potato"];
+
+    var_dump($str->fruit);
+
+
+
+
