5f796f94d2ca8dc0b44998077ada95f26ba1c330
Masud Rana <masudranaonline75@gmail.com>
2023-09-10 20:04:39 +0600

    week-1

---
+++ week-1/function/function.php
@@ -0,0 +1,43 @@
+<?php
+
+    // function shout() {
+
+    //     return "Masud Rana";
+    // }
+
+    // // echo shout();
+
+    // $str = shout();
+    // echo $str;
+
+
+    // function shout($massage) {
+    //     return $massage;
+    // }
+    // echo shout("Bangladesh");
+
+
+    function shout($massage = "Programming", $times = 6) {
+        echo str_repeat("$massage<br>", $times);
+
+    }
+    echo shout();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
