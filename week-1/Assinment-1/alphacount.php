b2373b5249767654ccaa0e42a517c6d557b0dad8
Masud Rana <masudranaonline75@gmail.com>
2023-09-16 10:47:13 +0600

    week-1

--- week-1/Assinment-1/alphacount.php
+++ week-1/Assinment-1/alphacount.php
@@ -1,4 +1,16 @@
 <?php
 
 
-    
+    $userInput = readline("Enter Sentence: ");
+
+
+    $count = 0;
+    $length = strlen($userInput);
+
+    for($i = 0; $i < $length; $i++){
+        if(ctype_alpha($userInput[$i])){
+            $count++;
+        }
+    }
+    print "Total Alphabet Character is: ".$count;
+
