e22babac35c9673f96dd83ad8f7019660a31faa9
Masud Rana <masudranaonline75@gmail.com>
2023-09-19 11:48:37 +0600

    week-2

--- week-2/traits.php
+++ week-2/traits.php
@@ -1,1 +1,50 @@
 <?php
+
+    class Vehicle {
+        // private Bike $bike;
+        // private Car $car;
+
+        use Kickable;
+
+        public function startBike() {
+            printf("Bike started, Masud Rana");
+        }
+        public function startCar() {
+            printf("Car started");
+        }
+
+        
+
+        
+    }
+
+    trait Kickable {
+        public function kick() {
+            printf("Bike kick");
+        }
+    }
+
+    trait Pushable {
+        public function push() {
+            printf("Car push");
+        }
+    }
+
+
+
+    class Bike extends Vehicle {
+
+        public function start() {
+            $object = new Vehicle();
+            $object->startBike();
+        }
+    }
+    class Car extends Vehicle {
+       
+    }
+
+    // $obj = new Vehicle();
+    // $obj->kick();
+
+    $obj = new Bike();
+   echo $obj->start();
