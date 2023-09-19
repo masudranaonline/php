ed5520f84ab4a8f320eee8f0aa8cf6c2747bee24
Masud Rana <masudranaonline75@gmail.com>
2023-09-19 11:21:55 +0600

    week-2

--- week-2/encapsulate.php
+++ week-2/encapsulate.php
@@ -1,1 +1,52 @@
 <?php
+
+    class Invoice {
+
+        private TaxCalculate $taxCalculate;
+
+        public function __construct()
+        {
+            $this->taxCalculate = new TaxCalculate();
+        }
+
+
+        public function getOrderTotal($order, array $items) {
+            $total = 0;
+
+            foreach($items as $item) {
+                $total+= ($item->pricr * $item->quantity);
+            }
+
+            $total += ($total * $this->taxCalculate->getTaxRate($order));
+
+            return $total;
+        }
+
+
+       
+    }
+
+    class TaxCalculate {
+        public function getTaxRate($order){
+
+            if($order->country == 'US') {
+                return $this->getUsTax($order);
+            }else if($order->country == 'EU') {
+                return $this->getEuTax($order->country);
+            }
+            return 0;
+        }
+
+        public function getUsTax($order) {
+            if($order->state == 'California') {
+                return 0.02;
+            }else {
+
+            }
+        }
+
+        public function getEuTax($country) {
+
+        }
+    }
+
